fa55e2f7bdb50a675db4c4c372a47daa02046248
dooyeon kang <dyhouse0517@gmail.com>
2026-05-01 19:41:00 +0900

    feat: Blueprint 헬퍼 라이브러리 추가

---
+++ Source/DataBridge/Private/DataBridgeLibrary.cpp
@@ -0,0 +1,87 @@
+#include "DataBridgeLibrary.h"
+#include "DataBridgeSubsystem.h"
+#include "DataBridgeLog.h"
+#include "Engine/GameInstance.h"
+#include "Kismet/GameplayStatics.h"
+
+UDataBridgeSubsystem* UDataBridgeLibrary::GetSubsystem(UObject* WorldContextObject)
+{
+	UGameInstance* GI = UGameplayStatics::GetGameInstance(WorldContextObject);
+	if (!GI)
+	{
+		UE_LOG(LogDataBridge, Warning, TEXT("DataBridgeLibrary: GameInstance not found"));
+		return nullptr;
+	}
+
+	UDataBridgeSubsystem* Subsystem = GI->GetSubsystem<UDataBridgeSubsystem>();
+	if (!Subsystem)
+	{
+		UE_LOG(LogDataBridge, Warning, TEXT("DataBridgeLibrary: DataBridgeSubsystem not found"));
+	}
+	return Subsystem;
+}
+
+void UDataBridgeLibrary::FetchTable(UObject* WorldContextObject, const FString& URL, UDataTable* TargetTable, EDataBridgeFormat Format)
+{
+	if (UDataBridgeSubsystem* S = GetSubsystem(WorldContextObject))
+	{
+		S->FetchTable(URL, TargetTable, Format);
+	}
+}
+
+void UDataBridgeLibrary::FetchCurveTable(UObject* WorldContextObject, const FString& URL, UCurveTable* TargetTable, EDataBridgeFormat Format)
+{
+	if (UDataBridgeSubsystem* S = GetSubsystem(WorldContextObject))
+	{
+		S->FetchCurveTable(URL, TargetTable, Format);
+	}
+}
+
+void UDataBridgeLibrary::FetchSource(UObject* WorldContextObject, FName SourceName)
+{
+	if (UDataBridgeSubsystem* S = GetSubsystem(WorldContextObject))
+	{
+		S->FetchSource(SourceName);
+	}
+}
+
+void UDataBridgeLibrary::FetchAllSources(UObject* WorldContextObject)
+{
+	if (UDataBridgeSubsystem* S = GetSubsystem(WorldContextObject))
+	{
+		S->FetchAllSources();
+	}
+}
+
+void UDataBridgeLibrary::SetEnvironment(UObject* WorldContextObject, EDataBridgeEnvironment NewEnvironment)
+{
+	if (UDataBridgeSubsystem* S = GetSubsystem(WorldContextObject))
+	{
+		S->SetEnvironment(NewEnvironment);
+	}
+}
+
+EDataBridgeEnvironment UDataBridgeLibrary::GetCurrentEnvironment(UObject* WorldContextObject)
+{
+	if (UDataBridgeSubsystem* S = GetSubsystem(WorldContextObject))
+	{
+		return S->GetCurrentEnvironment();
+	}
+	return EDataBridgeEnvironment::Local;
+}
+
+void UDataBridgeLibrary::InvalidateCache(UObject* WorldContextObject, FName SourceName)
+{
+	if (UDataBridgeSubsystem* S = GetSubsystem(WorldContextObject))
+	{
+		S->InvalidateCache(SourceName);
+	}
+}
+
+void UDataBridgeLibrary::InvalidateAllCache(UObject* WorldContextObject)
+{
+	if (UDataBridgeSubsystem* S = GetSubsystem(WorldContextObject))
+	{
+		S->InvalidateAllCache();
+	}
+}
---
+++ Source/DataBridge/Public/DataBridgeLibrary.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "DataBridgeTypes.h"
+#include "DataBridgeLibrary.generated.h"
+
+class UDataTable;
+class UCurveTable;
+
+UCLASS()
+class DATABRIDGE_API UDataBridgeLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "DataBridge", meta = (WorldContext = "WorldContextObject"))
+	static void FetchTable(UObject* WorldContextObject, const FString& URL, UDataTable* TargetTable, EDataBridgeFormat Format = EDataBridgeFormat::Json);
+
+	UFUNCTION(BlueprintCallable, Category = "DataBridge", meta = (WorldContext = "WorldContextObject"))
+	static void FetchCurveTable(UObject* WorldContextObject, const FString& URL, UCurveTable* TargetTable, EDataBridgeFormat Format = EDataBridgeFormat::Json);
+
+	UFUNCTION(BlueprintCallable, Category = "DataBridge", meta = (WorldContext = "WorldContextObject"))
+	static void FetchSource(UObject* WorldContextObject, FName SourceName);
+
+	UFUNCTION(BlueprintCallable, Category = "DataBridge", meta = (WorldContext = "WorldContextObject"))
+	static void FetchAllSources(UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintCallable, Category = "DataBridge", meta = (WorldContext = "WorldContextObject"))
+	static void SetEnvironment(UObject* WorldContextObject, EDataBridgeEnvironment NewEnvironment);
+
+	UFUNCTION(BlueprintPure, Category = "DataBridge", meta = (WorldContext = "WorldContextObject"))
+	static EDataBridgeEnvironment GetCurrentEnvironment(UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintCallable, Category = "DataBridge", meta = (WorldContext = "WorldContextObject"))
+	static void InvalidateCache(UObject* WorldContextObject, FName SourceName);
+
+	UFUNCTION(BlueprintCallable, Category = "DataBridge", meta = (WorldContext = "WorldContextObject"))
+	static void InvalidateAllCache(UObject* WorldContextObject);
+
+private:
+	static class UDataBridgeSubsystem* GetSubsystem(UObject* WorldContextObject);
+};
